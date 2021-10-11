b762c774c4d5367e8110d64477ada85e33e9d9a2
tina-chan <kassi_0123@kassi-hp.info>
2021-10-11 13:52:11 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/survivalsystem/SurvivalSystem.java
@@ -0,0 +1,17 @@
+package xyz.n7mn.dev.survivalsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
